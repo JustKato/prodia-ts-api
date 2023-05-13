a46d552deca1028f4476d35830a703de08a6387f
Daniel Legt <contact@danlegt.com>
2023-05-14 02:09:16 +0300

    Instant Generators Implemented two new functions for ease of use.

--- lib/Prodia.ts
+++ lib/Prodia.ts
@@ -1,3 +1,4 @@
+import { JobStatus } from "./types/GeneralTypes";
 import { CreateGenerationRequest, GenerationResponse } from "./types/GenerationTypes";
 import { ImageToImageRequest } from "./types/ImageToImageTypes";
 
@@ -21,6 +22,37 @@ export class Prodia {
     public createImageGeneration( generationRequest: CreateGenerationRequest ): Promise<GenerationResponse> {
         return new Promise<GenerationResponse>( async (_r, _e) => {
             const response = await fetch(`${PRODIA_BASE_URL}/v1/job`, {
+                method: `POST`,
+                cache: `no-cache`,
+                headers: this.getProdiaHeaders(),
+                redirect: `follow`,
+                referrerPolicy: `no-referrer`,
+                body: JSON.stringify(generationRequest),
+            })
+
+            // Make sure to check response status
+            if ( response.status > 299 || response.status < 200 ) {
+                return _e(new Error(`Api responded with ${response.status} ${response.statusText}`))
+            }
+
+            // Parse the response and return it
+            const responseJSON = await response.json();
+
+            // TODO: Implement some kind of check to make sure that the response JSON respects the GenerationResponse interface
+
+            // Return the response JSON
+            return _r(responseJSON as GenerationResponse);
+        })
+    }
+
+    /**
+     * Generate an image using the ImageToImage method.
+     * @param generationRequest The request parameters to send over
+     * @returns A GenerationResponse with the jobID
+     */
+    public createImageToImage( generationRequest: ImageToImageRequest ): Promise<GenerationResponse> {
+        return new Promise<GenerationResponse>( async (_r, _e) => {
+            const response = await fetch(`${PRODIA_BASE_URL}/v1/transform`, {
                 method: `POST`,
                 cache: `no-cache`,
                 headers: this.getProdiaHeaders(),
@@ -73,29 +105,41 @@ export class Prodia {
         })
     }
 
-    public createImageToImage( generationRequest: ImageToImageRequest ): Promise<GenerationResponse> {
-        return new Promise<GenerationResponse>( async (_r, _e) => {
-            const response = await fetch(`${PRODIA_BASE_URL}/v1/transform`, {
-                method: `POST`,
-                cache: `no-cache`,
-                headers: this.getProdiaHeaders(),
-                redirect: `follow`,
-                referrerPolicy: `no-referrer`,
-                body: JSON.stringify(generationRequest),
-            })
+    /**
+     * Generate an image from a text prompt
+     * @param generationRequest The generation parameters for the request
+     */
+    public generateText2Image( generationRequest: CreateGenerationRequest ): Promise<GenerationResponse> {
+        return new Promise( async (_r, _e) => {
+            let j = await this.createImageGeneration(generationRequest);
 
-            // Make sure to check response status
-            if ( response.status > 299 || response.status < 200 ) {
-                return _e(new Error(`Api responded with ${response.status} ${response.statusText}`))
+            // Now we wait for the image to process
+            let safetyWait = 25;
+            while ((j.status == JobStatus.GENERATING || j.status == JobStatus.QUEUED) && --safetyWait > 0 ) {
+                await new Promise((r) => setTimeout(r, 500));
+                j = await this.fetchImageGeneration(j.job);
             }
 
-            // Parse the response and return it
-            const responseJSON = await response.json();
+            return _r(j);
+        })
+    }
 
-            // TODO: Implement some kind of check to make sure that the response JSON respects the GenerationResponse interface
+    /**
+     * Generate an image from an image + prompt
+     * @param generationRequest The generation parameters for the request
+     */
+    public generateImage2Image( generationRequest: ImageToImageRequest ): Promise<GenerationResponse> {
+        return new Promise( async (_r, _e) => {
+            let j = await this.createImageToImage(generationRequest);
 
-            // Return the response JSON
-            return _r(responseJSON as GenerationResponse);
+            // Now we wait for the image to process
+            let safetyWait = 25;
+            while ((j.status == JobStatus.GENERATING || j.status == JobStatus.QUEUED) && --safetyWait > 0 ) {
+                await new Promise((r) => setTimeout(r, 500));
+                j = await this.fetchImageGeneration(j.job);
+            }
+
+            return _r(j);
         })
     }
 
